6d381f26d7653ea3a4297b001dda77095c44a980
Zac Adeboye <zacadeboye@gmail.com>
2025-12-18 20:12:57 +0100

    Changes to code after meeting with steve, fixed most warnings

--- client/file_client.c
+++ client/file_client.c
@@ -4,15 +4,26 @@
 #include <string.h>
 #include <unistd.h>
 
-#define PORT 8080
-#define SERVER_IP "127.0.0.1"
-#define BUFFER_SIZE 1024
+
+// used enum instead of define
+enum{
+  PORT = 8080,
+  BUFFER_SIZE = 1024,
+  PATH_LEN = 4096
+};
+
+// Like we talked about the possible errors that comes with using scanf. This function should get rid every character 
+// currently sitting in stdin until it finds a newline
+void clear_input_buffer() {
+    int get_rid = 0;
+    while ((get_rid = getchar()) != '\n' && get_rid != EOF) {}
+}
 
 int main() {
   int sock = 0;
   struct sockaddr_in serv_addr;
   char buffer[BUFFER_SIZE] = {0};
-  char filename[100];
+  char filename[BUFFER_SIZE];
 
   if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     printf("\n Socket creation error \n");
@@ -22,7 +33,7 @@ int main() {
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(PORT);
 
-  if (inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr) <= 0) {
+  if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
     printf("\nInvalid address / Address not supported \n");
     return -1;
   }
@@ -34,32 +45,47 @@ int main() {
 
   printf("Connected to server!\n");
   printf("Enter filename to download: ");
-  scanf("%99s", filename);
+  if (scanf("%99s", filename) != 1){
+    (void)fprintf(stderr, "Error reading input\n");
+        close(sock);
+        return -1;
+    } // value from scanf should be used (cmake warning) so I just implemented a simple check function
+
+  clear_input_buffer();
 
   send(sock, filename, strlen(filename), 0);
   printf("Request sent for '%s'. Waiting for data...\n", filename);
 
-  char output_path[200];
+  char output_path[PATH_LEN];
 
-  snprintf(output_path, sizeof(output_path), "../received_files/%s", filename);
+  int needed = snprintf(output_path, sizeof(output_path), "../received_files/%s", filename);
+  // dealing with cmake warning, value return by this function should be used
+  
+  if (needed < 0 || needed >= (int)sizeof(output_path)) {
+        (void)fprintf(stderr, "Error: Filename too long\n");
+        close(sock);
+        return -1;
+    }
 
   printf("Saving file to: %s\n", output_path);
 
-  FILE* fp = fopen(output_path, "wb");
-  if (fp == NULL) {
+  FILE* filepath = fopen(output_path, "wbe");
+  if (filepath == NULL) {
     perror(
         "Failed to open file for writing ");
     return -1;
   }
 
-  int bytes_received;
-  while ((bytes_received = read(sock, buffer, BUFFER_SIZE)) > 0) {
-    fwrite(buffer, 1, bytes_received, fp);
+  ssize_t bytes_received = 0; //read returns ssize_t, if read returned a huge number, when put into int it might chop off half the bits and 
+  // the remaining might be a negative number, our while loop would fail even though it was a success.
+  while ((bytes_received = read(sock, buffer, BUFFER_SIZE)) > 0) { // read returns ssize_t, long int
+    (void)fwrite(buffer, 1, (size_t)bytes_received, filepath); // fwrite expects size_t, read needs ssize_t to return -1, even though we wouldn't get
+    // to the line if read return -1, to silence cmake warning I still casted it. 
   }
 
   printf("File saved as '%s'\n", output_path);
 
-  fclose(fp);
+  (void)fclose(filepath);
   close(sock);
   return 0;
 }
--- server/file_server.c
+++ server/file_server.c
@@ -1,3 +1,4 @@
+#include <fcntl.h> 
 #include <netinet/in.h>
 #include <poll.h>
 #include <pthread.h>
@@ -8,12 +9,32 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define PORT 8080
-#define BACKLOG 10
-#define BUFFER_SIZE 1024
-#define PATH_BUFFER 1100
+enum {
+  PORT = 8080,
+  BACKLOG = 10,
+  BUFFER_SIZE = 1024, // sticking with this buffer size because it minimizes context switches when using recv and not reserving a significant large portion of the RAM,
+  //initially was confused after our meeting, and was going to alter the size but after checking on google this seems to be a "Goldilocks" number
+  // I learned memory is allocated in powers of 2 so 1024 (2^10), would probably make the process faster
+  PATH_BUFFER = 4096 // on a linux environment this is the path max, so even though I dont plan on using a file path that long, by matching the os limit
+  //i can garauntee the program can hadle anything my os can.
+}; // switched to using an enum like we talked about
 
-int shutdown_pipe[2];
+
+// implementation drawn from practice problem 15
+typedef struct {
+    int socket_fd;
+    int is_active; // 0 = Empty, 1 = Busy
+} client_slot_t;
+
+
+client_slot_t client_slots[BACKLOG]; 
+
+pthread_mutex_t slot_lock = PTHREAD_MUTEX_INITIALIZER;
+
+
+static int shutdown_pipe[2]; // cmake flag returned variable is non const and globally accessible, was going declare as const but that means we cant write to it,
+// so declare as static and now only file_server.c has access to it.
+
 
 void handle_sigint(int sig) {
   (void)sig;
@@ -22,24 +43,33 @@ void handle_sigint(int sig) {
 }
 
 void* client_handler(void* socket_desc) {
-  int sock = *(int*)socket_desc;
-  free(socket_desc);
+  client_slot_t* my_slot = (client_slot_t*)socket_desc;
+  int sock = my_slot->socket_fd;
 
   pthread_detach(pthread_self());
 
   char buffer[BUFFER_SIZE] = {0};
   char filename[BUFFER_SIZE] = {0};
 
-  ssize_t valread = recv(sock, filename, sizeof(buffer), 0);
+  ssize_t valread = recv(sock, filename, sizeof(filename), 0);
   if (valread <= 0) {
     close(sock);
+    pthread_mutex_lock(&slot_lock);
+    my_slot->is_active = 0;
+    pthread_mutex_unlock(&slot_lock);
     return NULL;
   }
 
   filename[strcspn(filename, "\r\n")] = 0;
 
   char filepath[PATH_BUFFER];
-  (void)snprintf(filepath, sizeof(filepath), "../test_files/%s", filename); 
+  int path_length = snprintf(filepath, sizeof(filepath), "../test_files/%s", filename); 
+
+  if (path_length < 0 || path_length >= (int)(sizeof(filepath))){
+    (void)fprintf(stderr, "Error: Filename too long.\n");
+    return NULL;
+  }; // introduced to check for overflow cases, snprintf will truncate if buffer isn't big enough, (solving one of the cmake warning)
+  // but if the truncated file name actually exist, we could end up reading the wrong file and not returnning an error
 
   printf("[Thread %lu] Client wants: %s\n", pthread_self(), filepath);
 
@@ -57,16 +87,63 @@ void* client_handler(void* socket_desc) {
 
   printf("[Thread %lu] Done. Closing socket.\n", pthread_self());
   close(sock);
+  pthread_mutex_lock(&slot_lock);
+  my_slot->is_active = 0; 
+  pthread_mutex_unlock(&slot_lock);
   return NULL;
+}
+
+//extracted code from while loop to create helper - cmake kept complaining about too if/for statements
+void accept_new_connection(int server_socket_id, struct sockaddr_in* address, socklen_t* addrlen) {
+    //Cmake warning: use accept4() because it allows SOCK_CLOEXEC. https://stackoverflow.com/questions/22304631/what-is-the-purpose-to-set-sock-cloexec-flag-with-accept4-same-as-o-cloexec 
+    // from my understanding from the reddit post, if my server ever spawns/produces a child process (like a helper program)
+    // the socket is automatically closed so client connection is not leaked to that process
+    int client_socket = accept4(server_socket_id, (struct sockaddr*)address, 
+                                addrlen, SOCK_CLOEXEC);
+    if (client_socket < 0) {
+        perror("Accept failed");
+        return;
+    }
+
+    printf("Accepted new connection.\n");
+
+    pthread_mutex_lock(&slot_lock); 
+    int found_slot = -1;
+    for (int i = 0; i < BACKLOG; i++) {
+        if (client_slots[i].is_active == 0) {
+            found_slot = i;
+            client_slots[i].is_active = 1; 
+            client_slots[i].socket_fd = client_socket;
+            break;
+        }
+    }
+    pthread_mutex_unlock(&slot_lock); 
+
+    if (found_slot != -1) {
+        pthread_t thread_id = 0;
+        if (pthread_create(&thread_id, NULL, client_handler, &client_slots[found_slot]) != 0) {
+            perror("Could not create thread");
+            pthread_mutex_lock(&slot_lock); 
+            client_slots[found_slot].is_active = 0;
+            pthread_mutex_unlock(&slot_lock);
+            close(client_socket);
+        }
+    } else {
+        (void)fprintf(stderr, "Server full: Rejecting client.\n");
+        close(client_socket);
+    }
 }
 
 int main() {
   int server_socket_id = 0;
-  int client_socket = 0;
   struct sockaddr_in address;
-  int addrlen = sizeof(address);
+  socklen_t addrlen = sizeof(address);
 
-  if (pipe(shutdown_pipe) < 0) { 
+  // Cmake warning warned to use pipe2() instead of pipe(), by default when we run a new program on the same
+  // server, it inherits every file descriptor so another executable could write to this pipe, pipe2() and a flag (O_CLOEXEC) could tell the OS if i run another program
+  // close this pipe. AI and https://pubs.opengroup.org/onlinepubs/9799919799/functions/pipe.html helped me understand this
+
+  if (pipe2(shutdown_pipe, O_CLOEXEC) < 0) { 
     perror("Pipe failed");
     return EXIT_FAILURE; 
   }
@@ -99,7 +176,7 @@ int main() {
     return EXIT_FAILURE; 
   }
 
-  printf("Server running (Ctrl+C to stop)...\n");
+  printf("Server running (Ctrl+C to stop)\n");
 
   struct pollfd fds[2];
 
@@ -110,7 +187,7 @@ int main() {
   fds[1].events = POLLIN;
 
   while (1) {
-    printf("Waiting for activity...\n");
+    printf("Waiting for activity\n");
 
     int activity = poll(fds, 2, -1);
 
@@ -118,37 +195,24 @@ int main() {
       continue;
     }
 
-
-    if (fds[1].revents & POLLIN) { 
-      printf("\nReceived shutdown signal! Cleaning up...\n");
+    // Cmake warning use of unsigned integer with binary bitwise operator
+    // after looking at https://man7.org/linux/man-pages/man2/poll.2.html for the data type of revents in the struct pollfd,
+    // revents is a short (signed 2 byte int), POLLIN is a raw bundle of bits (google search, short top description), which is,
+    // unsigned int, so we can cast revents to unsigned to fix.
+    if ((unsigned)fds[1].revents & (unsigned)POLLIN) { 
+      printf("\nReceived shutdown signal.\n");
       break;
     }
 
-    if (fds[0].revents & POLLIN) { 
-      if ((client_socket = accept(server_socket_id, (struct sockaddr*)&address, 
-                                  (socklen_t*)&addrlen)) < 0) {
-        perror("Accept failed");
-        continue;
-      }
-
-      printf("Accepted new connection.\n");
-
-      pthread_t thread_id = 0; 
-      int* new_sock = malloc(sizeof(int));
-      *new_sock = client_socket;
-
-      if (pthread_create(&thread_id, NULL, client_handler, (void*)new_sock) != 0) {
-        perror("Could not create thread");
-        free(new_sock);
-        close(client_socket);
-      }
+    if ((unsigned)fds[0].revents & (unsigned)POLLIN) { 
+      accept_new_connection(server_socket_id, &address, &addrlen);
     }
   }
 
   close(server_socket_id);
   close(shutdown_pipe[0]);
   close(shutdown_pipe[1]);
-  printf("Server shutdown complete. Bye!\n");
+  printf("Server shutdown complete.\n");
 
   return 0;
 }
